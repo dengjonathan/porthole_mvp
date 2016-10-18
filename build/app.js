cc5521745e45a322f8804e9720e2ee27f99332d7
jonathan.deng <dengjonathan@gmail.com>
2016-10-18 16:47:27 -0400

    rebuilt

--- build/app.js
+++
@@ -1,6 +0,0 @@
-const createServer = require('./server.js')
-const port = (process.env.PORT || 8080)
-
-const app = createServer().listen(port);
-
-console.log(`Listening at http://localhost:${port}`);
