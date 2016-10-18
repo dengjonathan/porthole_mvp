9ad749584f8ef20fd160b0033e259f6b5d359650
jonathan.deng <dengjonathan@gmail.com>
2016-10-18 13:30:12 -0400

    added https redirect middleward

--- build/server.js
+++ build/server.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const express = require('express');
 
+const forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+ };
+
 const createServer = () => {
   const app = express();
   const indexPath = path.join(__dirname, 'index.html');
@@ -8,6 +15,7 @@ const createServer = () => {
 
   app.use('/static', publicPath);
   app.get('/', function (_, res) { res.sendFile(indexPath) });
+  app.use(forceSsl);
 
   return app;
 }
