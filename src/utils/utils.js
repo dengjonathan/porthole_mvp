d8b88bbd3252095ddb47fdb4988cc43ca50587cb
jonathan.deng <dengjonathan@gmail.com>
2016-10-18 13:09:06 -0400

    changed api request to https

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const NYT_URL = 'http://api.nytimes.com/svc/topstories/v2/world.json?api-key=29e640f958d54b019d58392b1e8bb3a0'
+const NYT_URL = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=29e640f958d54b019d58392b1e8bb3a0'
 
 // converts results from NYT api into object shape accepted by this app
 const convertNYT = (result) => {
