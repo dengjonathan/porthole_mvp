64500d4769803eabe791eb4ef6b70422440e0692
jonathan.deng <dengjonathan@gmail.com>
2016-10-17 20:34:26 -0400

    can make get request to nyt api

---
+++ src/apiRequest.js
@@ -0,0 +1,8 @@
+var axios = require('axios');
+
+const NYT_URL = 'http://api.nytimes.com/svc/topstories/v2/world.json?api-key=29e640f958d54b019d58392b1e8bb3a0'
+
+axios.get(NYT_URL)
+  .then((res) => {
+    console.log(res.data.results);
+  });
